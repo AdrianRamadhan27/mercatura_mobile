b1213bf2703202ea233991d169db33cbe1cafab1
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-12-10 11:02:59 +0700

    Authentication Done

---
+++ lib/custom_widgets/mydrawer.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/material.dart';
+import 'package:mercatura/main.dart';
+import 'package:mercatura/pages/login_page.dart';
+import 'package:mercatura/api/pengguna_api.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+    // TODO: implement build
+    return Drawer(
+      child: Column(
+        children: [
+          // Menambahkan clickable menu
+          ListTile(
+            title: const Text('Home'),
+            onTap: () {
+              // Route menu ke halaman utama
+              Navigator.of(context).pushReplacementNamed("/home");
+            },
+          ),
+          ListTile(
+            title: const Text('Login'),
+            onTap: () {
+              // Route menu ke halaman utama
+              Navigator.of(context).pushReplacementNamed("/login");
+            },
+          ),
+          ListTile(
+            title: const Text('Logout'),
+            onTap: () {
+              // Route menu ke halaman utama
+              logout(request);
+              const snackBar = SnackBar(content: Text("Berhasil logout!"));
+              scaffoldMessenger.showSnackBar(snackBar);
+              Navigator.of(context).pushReplacementNamed("/login");
+
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
