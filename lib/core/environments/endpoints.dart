5fec27b05f906910a897c1ae4d439e495ce8fa9c
katrinagisellaui <katrina.gisella@ui.ac.id>
2022-12-12 10:52:23 +0700

    changing endpoint
    .

--- lib/core/environments/endpoints.dart
+++ lib/core/environments/endpoints.dart
@@ -1,6 +1,6 @@
 class Endpoint {
   static const String baseUrl = 'http://0.0.0.0:8000/';
   static const String kritikSaran = '$baseUrl/kritiksaran/json/';
-  static const String createKritikSaranJson = '$baseUrl/kritiksaran/create_json/';
+  static const String createKritikSaranJson = '$baseUrl/kritiksaran/create_post_json/';
 //   static const String createPostJson = '$baseUrl/artikel/create_json/';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:pbp_django_auth/pbp_django_auth.dart';
